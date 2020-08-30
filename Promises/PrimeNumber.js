048a9d30ac75ea37d9151628780d43d7010a88c2
Keerthi-M <keermano2017@gmail.com>
2020-08-30 19:40:32 +0530

    Date class of js is used to get millisecond of the instance

--- Promises/PrimeNumber.js
+++ Promises/PrimeNumber.js
@@ -3,10 +3,15 @@
 
 let p = new Promise((resolve, reject) => {
 
-// Camel spacing naming convention has been used for the variable and function names
+// Camel spacing naming convention has been used for the variable and function names declarations.
 const primeNumber = [2,3,5,7,11,13,11,17,19];
-var milliSecond = 4;
-if ( primeNumber[4] % milliSecond ==0)
+
+// Date object 'd' is created with Date class and the parameters inside the Date class are in the format ("Month date, year hour:minutes:seconds:milliseconds").
+var d = new Date("August 30, 2020 01:15:00:550");
+
+// getMilliseconds() method returns the milliseconds (from 0 to 999) of the specified date and time.
+var milliSec = d.getMilliseconds();
+if ( primeNumber[4] % milliSec ==0)
 {
 	resolve('Sucess !')
 }
@@ -16,13 +21,13 @@ else
 }
 })
 
-// then method is going to be called when the promise resolves successfully 
+// then method is going to be called when the promise resolves successfully.
 p.then( (message) => {
 console.log(' Millisecond of the instance is divisible by the fourth index of the primeNumber Array. Therefore, the promise would like to return' +
  message)
 })
 
-// catch method is going to be called when the promise is rejected or fails
+// catch method is going to be called when the promise is rejected or fails.
 .catch( (message) => {
 console.log(' Millisecond of the instance is not divisible by the fourth index of the primeNumber Array. Therefore, the promise would like to say :  ' + message)
 })
